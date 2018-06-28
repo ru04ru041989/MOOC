68371629aea6ddce3295b6e6306736e286eb0351
Jamie Chen <ru04ru041989@gmail.com>
2018-06-28 16:42:50 -0400

    Update file 'hash_function' Add function 'hashtable_get_bucket'
    refine function description

--- hash_function.py
+++ hash_function.py
@@ -1,10 +1,9 @@
 # hash function
 
 def hash_string(keyword,buckets):
-    '''
-    takes as inputs a keyword (string) and a number of buckets,
-    returns a number representing the bucket for that keyword.
-    '''
+    '''takes as inputs a keyword (string) and a number of buckets,
+    returns a number representing the bucket for that keyword.'''
+    
     result = 0
     for word in keyword:
         result = result + (ord(word) % buckets)
@@ -13,12 +12,20 @@ def hash_string(keyword,buckets):
 
 
 def make_hashtable(nbuckets):
-    '''
-    Creating an Empty Hash Table
-    returns an empty hash table with nbuckets empty buckets.
-    '''
+    '''Creating an Empty Hash Table
+    returns an empty hash table with nbuckets empty buckets.'''
+    
     Htable = []
     for i in range(0,nbuckets):
         Htable.append([])
     
     return Htable
+
+
+def hashtable_get_bucket(htable,keyword):
+    '''two inputs - a hashtable, and a keyword
+    returns the bucket where the keyword could occur'''
+    
+    h_index = hash_string(keyword,len(htable))
+    
+    return htable[h_index]
